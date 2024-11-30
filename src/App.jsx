e2e2026f15e1e8afe54285473b780355ab629a30
Livea Brito <159591382+LiveaBrito@users.noreply.github.com>
2024-11-30 05:08:43 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import ParagrafoBool from './components/ParagrafoBool'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <ParagrafoBool isVisible = {true}></ParagrafoBool>
+      <ParagrafoBool isVisible = {false}></ParagrafoBool>
+      <ParagrafoBool isVisible = {true}></ParagrafoBool>
+      <ParagrafoBool isVisible = {false}></ParagrafoBool>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ParagrafoBool.jsx
@@ -0,0 +1,7 @@
+function ParagrafoBool (props){
+
+    return props.isVisible ? <p>Esse paragrafo só aparecerá se caso a condição for igual a true</p> : " ";
+
+}
+
+export default ParagrafoBool;
